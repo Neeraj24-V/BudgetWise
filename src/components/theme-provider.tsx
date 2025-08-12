80b7cf37f3f84cb4b9ee296eadc396642a331bad
Neeraj Virodula <Virodulaneeraj@gmail.com>
2025-08-12 17:20:07 +0000

    theme is not changing when I select tokyo night

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -5,22 +5,15 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const { forcedTheme } = props
   const [mounted, setMounted] = React.useState(false)
 
   React.useEffect(() => {
     setMounted(true)
   }, [])
-  
-  const theme = React.useMemo(() => {
-    if (forcedTheme) return forcedTheme
-    if (mounted) return props.theme
-    return undefined
-  }, [mounted, forcedTheme, props.theme]);
 
-  return (
-    <NextThemesProvider {...props} theme={theme}>
-      {children}
-    </NextThemesProvider>
-  )
+  if (!mounted) {
+    return <>{children}</>
+  }
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
