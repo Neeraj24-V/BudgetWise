dcd2bd198e9d108bfb386d5dc0771b2c69d94518
Neeraj Virodula <Virodulaneeraj@gmail.com>
2025-08-15 19:05:29 +0000

    remove anurati and get a cool looking unique font of your choice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -9,11 +9,7 @@ import { CurrencyProvider } from "@/context/currency-context";
 import { AuthProvider } from "@/context/auth-context";
 
 
-const anurati = localFont({
-  src: '../fonts/anurati.otf',
-  variable: '--font-sans',
-  display: 'swap',
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "BudgetWise - The Smart Expense Tracker",
@@ -27,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background font-sans antialiased", anurati.variable)}>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
         <AuthProvider>
             <ThemeProvider
                 attribute="class"
